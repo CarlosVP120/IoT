767ab18adb25d7ec6a565f083376dd4ea69df983
diegopartida22 <A01641113@tec.mx>
2022-11-08 12:42:55 -0600

    hola

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/Header.js/Header';
 
 function App() {
   return (
     <div className="App">
-        <h1>IoT</h1>
+        <Header/>
     </div>
   );
 }
---
+++ src/components/Header.js/Header.js
@@ -0,0 +1,9 @@
+const Header = (props) => {
+  return (
+    <header>
+      <h1>Iot</h1>
+    </header>
+  );
+}
+
+export default Header;
